306746b8fca1859a16f784c6ac7eb4f321f2159d
Justin Schneck <jschneck@mac.com>
2016-11-09 19:39:45 -0500

    updated for nerves ~> 0.4.0

--- nerves.exs
+++ nerves.exs
@@ -11,16 +11,18 @@ config :nerves_system_bbb, :nerves_env,
   mirrors: [
     "https://github.com/nerves-project/nerves_system_bbb/releases/download/v#{version}/nerves_system_bbb-v#{version}.tar.gz",
     "https://s3.amazonaws.com/nerves/artifacts/nerves_system_bbb-#{version}.tar.gz"],
-  build_platform: Nerves.System.Platforms.BR,
-  build_config: [
+  platform: Nerves.System.BR,
+  platform_config: [
     defconfig: "nerves_defconfig",
-    package_files: [
-      "bbb-busybox.config",
-      "rootfs-additions",
-      "fwup.conf",
-      "post-createfs.sh",
-      "uboot",
-      "uboot-script.cmd",
-      "linux"
-    ]
+  ],
+  checksum: [
+    "linux",
+    "rootfs-additions",
+    "uboot",
+    "bbb-busybox.config",
+    "fwup.conf",
+    "nerves_defconfig",
+    "nerves.exs",
+    "post-createfs.sh",
+    "uboot-script.cmd",
   ]
